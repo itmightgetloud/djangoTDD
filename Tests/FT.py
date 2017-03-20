8ca34a473978d165f4116419995a3440f05f9e16
itmightgetloud <ksnopek@gmail.com>
2017-03-20 16:32:40 +0000

    added pyvirtualdisplay lib to be able to test in terminal env

--- Tests/FT.py
+++ Tests/FT.py
@@ -1,6 +1,15 @@
+from pyvirtualdisplay import Display
 from selenium import webdriver
+
+display = Display(visible=0,size=(800,600))
+display.start()
 
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
+
 assert 'Django' in browser.title
+
+
+browser.quit()
+display.stop()
