c27d62191596973356d08d229e093df5cdac17d0
itmightgetloud <ksnopek@gmail.com>
2017-03-27 22:13:12 +0000

    FT checks we can input a todo item

--- tests/FT.py
+++ tests/FT.py
@@ -1,5 +1,7 @@
 from pyvirtualdisplay import Display
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,7 +16,22 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('To-Do', self.browser.title)
 		#assert 'To-Do' in browser.title
-		self.fail("Finish the test")
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter to do item'
+		)
+
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for rown in rows)
+		)
+		self.fail('Finish the test')
+		#tofinish 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
 #print(browser.title)
