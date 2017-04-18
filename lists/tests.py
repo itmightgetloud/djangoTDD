25572856e732c71207a239f43b3cfbc9dc7b172b
itmightgetloud <ksnopek@gmail.com>
2017-04-18 04:46:39 +0000

    added each item associated with list

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -39,16 +39,24 @@ class HomePageTest(TestCase):
 		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
 		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 		
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+		
 		first_item = Item()
 		first_item.text = 'The first list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Second Item'
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -56,13 +64,16 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_item.text, first_saved_item.text)
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_item.text, second_saved_item.text)
+		self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 	
 	def test_display_all_items(self):
-		Item.objects.create(text="item1")
-		Item.objects.create(text="item2")
+		list_ = List.objects.create()
+		Item.objects.create(text="item1", list = list_)
+		Item.objects.create(text="item2", list = list_)
 		
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		
@@ -83,7 +94,6 @@ class NewListTest(TestCase):
 		
 	def test_redirects_after_POST(self):
 		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-		#self.assertEqual(response.status_code, 302)
 		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 #Create your tests here.
