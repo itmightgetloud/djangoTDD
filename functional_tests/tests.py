6fc8fffdaeea3c59b434f110f439c18f555b3c82
itmightgetloud <ksnopek@gmail.com>
2017-05-15 02:29:50 +0200

    modified FT for layout testing

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,5 @@
 from django.test import LiveServerTestCase
-from pyvirtualdisplay import Display
+#from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -10,13 +10,13 @@ MAX_WAIT = 10
 class NewVisitorTest(LiveServerTestCase):
 	
 	def setUp(self):
-		self.display = Display(visible=0, size=(1024,768))
-		self.display.start()
+		#self.display = Display(visible=0, size=(1024,768))
+		#self.display.start()
 		self.browser = webdriver.Firefox()
 	
 	def tearDown(self):
 		self.browser.quit()
-		self.display.stop()
+		#self.display.stop()
 	"""
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
@@ -86,18 +86,26 @@ class NewVisitorTest(LiveServerTestCase):
 		second_user_list_url = self.browser.current_url
 		self.assertRegex(second_user_list_url, '/lists/.+')
 		self.assertNotEqual(first_user_list_url, second_user_list_url)
-		
-		def test_layout_and_styling(self):
-			self.browser.get(self.live_server_url)
-			self.browser.set_window_size(1024,768)
-			
-			inputbox = self.browser.find_element_by_id('id_new_item')
-			self.assertAlmostEqual(
-				inputbox.location['x'] + inputbox.size['width'] / 2,
-				512,
-				delta=10
-				)
-		
+	def test_layout_and_styling(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			(inputbox.location['x'] + inputbox.size['width'] / 2),
+			512,
+			delta = 10
+			)	
+
+		inputbox.send_keys('testtest')
+		inputbox.send_keys(ENTER)
+		self.wait_for_row_in_list_table('1: testtest')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			(inputbox.location['x'] + inputbox.size['width'] / 2),
+			512,
+			delta = 10
+			)
 """	
 	def test_can_add_second_item_and_both_are_visible(self):
 		self.browser.get('http://localhost:8000')
