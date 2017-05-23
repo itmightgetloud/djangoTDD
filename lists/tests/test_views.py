60bf7672eba60ceedb9f7c1233ecadcc63141563
itmightgetloud <ksnopek@gmail.com>
2017-05-22 17:27:46 -0700

    Split unit tests to separate file for testing Views and Models

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,30 +1,8 @@
 from django.test import TestCase
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
 from lists.models import Item, List
-from lists.views import home_page
 
-
-"""
-class SmokeTest(TestCase):
-	def test_bad_maths(self):
-		self.assertEqual(1 + 1, 3)
-"""
 class HomePageTest(TestCase):
 	
-	"""
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-	
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do</title>', html)
-		self.assertTrue(html.strip().endswith('</html>'))
-	"""
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
@@ -35,34 +13,6 @@ class HomePageTest(TestCase):
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')		
 		
-class ListAndItemModelTest(TestCase):
-
-	def test_saving_and_retrieving_items(self):
-		list_ = List()
-		list_.save()
-		
-		first_item = Item()
-		first_item.text = 'The first list item'
-		first_item.list = list_
-		first_item.save()
-
-		second_item = Item()
-		second_item.text = 'Second Item'
-		second_item.list = list_
-		second_item.save()
-
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, list_)
-
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_item.text, first_saved_item.text)
-		self.assertEqual(first_saved_item.list, list_)
-		self.assertEqual(second_item.text, second_saved_item.text)
-		self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 	
