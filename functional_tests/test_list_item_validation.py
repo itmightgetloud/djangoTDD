f7db38d646f11fad788b20582282a70a277a426e
itmightgetloud <ksnopek@gmail.com>
2017-05-22 17:15:13 -0700

    Unit tests moved to the single file

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,5 +4,32 @@ from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
 	def test_cannot_add_empty_list_items(self):
-		self.fail('write me!')
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id("id_new_item")
+		inputbox.send_keys(Keys.ENTER)
+
+		self.wait_for(
+			lambda:self.assertEqual(
+				self.browser.find_element_by_css_selector('.has-error').text,
+				"You can't add empty list item"
+			)
+		)
+
+		inputbox.send_keys('This time not empty')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1 This time not empty')
+
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for(
+			lambda:self.assertEqual(
+				self.browser.find_element_by_css_selector('.has-error').text,
+				"You can't add empty list item"
+			)
+		)
+
+		inputbox.send_keys("Another not empty entry")
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1 This time not empty')
+		self.wait_for_row_in_list_table('2 Another not empty entry')
+
 
