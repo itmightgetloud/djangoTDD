d736805db13213b7d69921097ef48755ca218731
itmightgetloud <ksnopek@gmail.com>
2017-05-22 21:43:05 -0700

    adjust view to model validation

--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import redirect, render
+from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
 def home_page(request):
-	"""
-	if request.method == 'POST':
-		Item.objects.create(text = request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
-	"""
 	return render(request, 'home.html')
+
 def new_list(request):
 	list_ = List.objects.create()
-	Item.objects.create(text = request.POST['item_text'], list = list_)
+	item = Item.objects.create(text = request.POST['item_text'], list = list_)
+	try:
+		item.full_clean()
+		item.save()
+	except ValidationError:
+		list_.delete()
+		error = "You can't add empty list item"
+		return render(request, 'home.html', {"error": error})
 	return redirect(f'/lists/{list_.id}/')
 
 def view_list(request, list_id):
