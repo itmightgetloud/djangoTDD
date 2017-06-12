85da15e63f41bcb3cc10458be1cc90db1b2fcdee
itmightgetloud <ksnopek@gmail.com>
2017-06-11 17:43:17 -0700

    added simple redirect and wired up urls from accounts to superlists

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+
+def send_login_email(request):
+	return redirect('/')
+
+
 
 # Create your views here.
