c80778f495cbef1dbffa3c679dcb3f876d8950b9
itmightgetloud <ksnopek@gmail.com>
2017-06-19 19:20:59 -0700

    added owner to List model to optionally assign list to an email account

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
+
+User = get_user_model()
 
 class ItemModelTest(TestCase):
 	
@@ -27,6 +30,14 @@ class ListModelTest(TestCase):
 	def test_get_absolute_url(self):
 		list_ = List.objects.create()
 		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
+	def test_lists_can_have_owners(self):
+		user = User.objects.create(email='a@b.com')
+		list_ = List.objects.create(owner=user)
+		self.assertIn(list_, user.list_set.all())
+
+	def test_lists_dont_need_owner(self):
+		List.objects.create()
 
 	def test_duplicate_items_are_invalid(self):
 		list_ = List.objects.create()
